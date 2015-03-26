5310fc03cb19ed6eb85f7e66b7df2c05ae4544c8
Marc!! <marc.bastien@gmail.com>
2015-03-26 01:11:52 +0000

    WHAT: Added data.js

---
+++ data/data.js
@@ -0,0 +1,14 @@
+{
+    "things" : [
+        {"id" : 1, "name" : "Rock"},
+        {"id" : 2, "name" : "Paper"},
+        {"id" : 3, "name" : "Scissors"},
+        {"id" : 4, "name" : "Foo"}
+    ], 
+    "people" : [
+        {"id" : 1, "name" : "Larry"},
+        {"id" : 2, "name" : "Curly"},
+        {"id" : 3, "name" : "Moe"},
+        {"id" : 4, "name" : "Mike"}
+    ]
+}
--- server.js
+++ server.js
@@ -1,64 +1,113 @@
 var express = require('express');
+var fs = require('fs');
+var JSON = require('JSON');
+var _ = require('underscore');
 
 var app = express();
 
 app.set("view engine", "jade"); // prevent Error: No default engine
 
-var things = [
-    {id : 1, name : "Rock"},
-    {id : 2, name : "Paper"},
-    {id : 3, name : "Scissors"}
+
+var tabs = [
+    {title : "Home", name : ""},
+    {title : "People", name : "people"},
+    {title : "Things", name : "things"},
 ];
 
-var people = [
-    {id : 1, name : "Larry"},
-    {id : 2, name : "Curly"},
-    {id : 3, name : "Moe"}
-];
+// var things = [
+//     {id : 1, name : "Rock"},
+//     {id : 2, name : "Paper"},
+//     {id : 3, name : "Scissors"}
+// ];
+
+// var people = [
+//     {id : 1, name : "Larry"},
+//     {id : 2, name : "Curly"},
+//     {id : 3, name : "Moe"}
+// ];
 
 app.locals.pretty = true; // make source pretty
 
+var _data;
+app.use(function(req, res, next){
+    if(_data){
+        res.locals.data = _data;
+        return next();
+    }
+    fs.readFile("data/data.js", function(err, dataStream){
+        if(err){
+            return next(err);
+        };
+        try{
+            _data = JSON.parse(dataStream.toString());
+            res.locals.data = _data;
+            next();
+            // console.log(data);
+            // res.render("things/things", { things : data.things, tabs : tabs, selected : "Things"}); 
+        } catch(e) {
+            next(e);
+        }
+    });
+});
+
 app.get("/", function(req, res){ // request, response
-    res.render("index");
+    res.render("index", {tabs : tabs, selected : "Home"});
     
 });
 
-app.get("/things", function(req, res){ // request, response
-    console.log("received request : " + req);
-    //res.send("<html><body><h1>hello world.  rand = " + Math.random() + "</h1></body></html>");
-    res.render("things", { xmessage : "Hello World" , things : things, tab : "things"});
+app.get("/things", function(req, res, next){ // request, response
     
+    // fs.readFile("data/data.js", function(err, dataStream){
+    //     if(err){
+    //         return next(err);
+    //     };
+        try{
+            // var data = JSON.parse(dataStream.toString());
+            // console.log(data);
+            res.render("things/things", { things : res.locals.data.things, tabs : tabs, selected : "Things"}); 
+        } catch(e) {
+            next(e);
+        }
+    // });
+    
+});
+
+app.use(function(req, res, next){
+    res.locals.tabs = tabs;
+    next();
 });
 
 app.get("/things/:id", function(req, res){ // request, response
-    var thing;
-    for(var i = 0; i < things.length; i++){
-        if(req.params.id == things[i].id){
-            thing = things[i];
-            break;
-        };
-    };
-    res.render("thing", { xmessage : "Hello World" , thing : thing});
+    var thing = _.find(res.locals.data.things, function(thing){
+        return thing.id == req.params.id;
+    });
+    // for(var i = 0; i < res.locals.data.things.length; i++){
+    //     if(req.params.id == res.locals.data.things[i].id){
+    //         thing = res.locals.data.things[i];
+    //         break;
+    //     };
+    // };
+    res.render("things/thing", { thing : thing, tabs : tabs, selected : "Things"});
     
 });
 
 app.get("/people", function(req, res){ // request, response
     console.log("received request : " + req);
     //res.send("<html><body><h1>hello world.  rand = " + Math.random() + "</h1></body></html>");
-    res.render("people", { things : people, tab : "people"});
+    res.render("people/people", { things : res.locals.data.people, tabs : tabs, selected : "People"});
     
 });
 
 app.get("/people/:id", function(req, res){ // request, response
-    var person;
-    for(var i = 0; i < things.length; i++){
-        if(req.params.id == people[i].id){
-            person = people[i];
-            break;
-        };
-    };
-    res.render("person", { thing : person});
+    var person = _.find(res.locals.data.people, function(person){
+        return person.id == req.params.id;
+    });
+    res.render("people/person", { thing : person, tabs : tabs, selected : "People"});
     
+});
+
+app.use(function(err, req, res, next){
+    res.send("Error page : " + err);
 });
 
 app.listen(process.env.PORT);
